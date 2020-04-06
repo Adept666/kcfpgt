8fa330291020024fb305214d61fd18a604622d92
Igor Ivanov <adept.666.1993@gmail.com>
2020-04-06 15:56:35 +0600

    Printing to files was configured

--- kcfpgt.py
+++ kcfpgt.py
@@ -1,10 +1,17 @@
-print('KiCad footprint generation tool')
+print('KiCad Footprint Generation Tool')
 print('Enter FP type:')
 series = input()
 
 if series == 'DIP':
-	pass
+	f = open('P-DIP-28.kicad_mod', 'w')
+	f.write(')')
+	f.close()
 elif series == 'SO':
-	pass
+	f = open('P-SO-28W.kicad_mod', 'w')
+	f.write(')')
+	f.close()
+	f = open('P-SO-28X.kicad_mod', 'w')
+	f.write(')')
+	f.close()
 else:
 	print('No matches')
