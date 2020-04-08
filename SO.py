523a34683b5a98933f7e6bf397c60cdbcbaf7c09
Igor Ivanov <adept.666.1993@gmail.com>
2020-04-08 17:41:12 +0600

    Draft header creation functions were written

--- SO.py
+++ SO.py
@@ -1,4 +1,15 @@
 def head(f, n, w = ''):
+	#! There is no suffix processing yet
+	timestamp = '00000000'	#!
+	ref_y = 0	#!
+	val_y = 0	#!
+	f.write('(module P-SO-%02d (layer F.Cu) (tedit %s)\n' % (n, timestamp))
+	f.write('  (fp_text reference REF** (at 0 %.3f) (layer F.SilkS)\n' % ref_y)
+	f.write('    (effects (font (size 1 1) (thickness 0.2)))\n')
+	f.write('  )\n')
+	f.write('  (fp_text value P-SO-%02d (at 0 %.3f) (layer F.Fab) hide\n' % (n, val_y))
+	f.write('    (effects (font (size 1 1) (thickness 0.2)))\n')
+	f.write('  )\n')
 	return 0
 
 def F_SilkS(f, n, w = ''):
