a15b795111bae33b7c00283cdf05153394f775fc
Igor Ivanov <adept.666.1993@gmail.com>
2020-04-08 14:55:58 +0600

    Pads creation functions were written

--- DIP.py
+++ DIP.py
@@ -11,6 +11,23 @@ def F_CrtYd(f, n, w = ''):
 	return 0
 
 def pads(f, n, w = ''):
+	if w == '':
+		c = 7.62
+	elif w == 'L':
+		c = 10.16
+	elif w == 'X':
+		c = 15.24
+	e = 2.54
+	for i in range(1, n + 1):
+		shape = 'circle' if i == 1 else 'rect'
+		if i <= n / 2:
+			x = -(c / 2)
+			y = -e * (0.25 * n + 0.5 - i)
+		else:
+			x = c / 2
+			y = e * (0.75 * n + 0.5 - i)
+		line = '  (pad ' + str(i) + ' thru_hole ' + shape + ' (at ' + str(x) + ' ' + str(y) + ') (size 2 2) (drill 1) (layers *.Cu *.Mask))\n'
+		f.write(line)
 	return 0
 
 def DIP_create(n, w = ''):
--- SO.py
+++ SO.py
@@ -11,6 +11,24 @@ def F_CrtYd(f, n, w = ''):
 	return 0
 
 def pads(f, n, w = ''):
+	if w == '':
+		c = 5.20
+	elif w == 'A':
+		c = 6.10
+	elif w == 'W':
+		c = 9.20
+	elif w == 'X':
+		c = 10.80
+	e = 1.27
+	for i in range(1, n + 1):
+		if i <= n / 2:
+			x = -(c / 2)
+			y = -e * (0.25 * n + 0.5 - i)
+		else:
+			x = c / 2
+			y = e * (0.75 * n + 0.5 - i)
+		line = '  (pad ' + str(i) + ' smd rect (at ' + str(x) + ' ' + str(y) + ') (size 2.2 0.6) (layers F.Cu F.Mask))\n'
+		f.write(line)
 	return 0
 
 def SO_create(n, w = ''):
